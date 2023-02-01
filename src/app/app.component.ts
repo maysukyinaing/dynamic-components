154088bfd568c82d95b9a18d774bfaa8524b341b
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-02-01 22:37:20 +0630

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SocialCardType} from "./constants/social-card-type";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dynamic-components';
+
+  selectedCardType!:SocialCardType;
+  cardTypes = SocialCardType;
+
+  setCardType(type:SocialCardType) {
+    this.selectedCardType = type
+  }
 }
